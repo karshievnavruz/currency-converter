b2065b316b129bbf8c6cad25ae9f79882439347b
karshievnavruz <qarshiyevnavruz7777@gmail.com git config --global user.email qarshiyevnavruz7777@gmail.com>
2023-02-11 11:19:55 +0500

    usd

--- app.py
+++ app.py
@@ -1,14 +1,21 @@
 from flask import Flask, request
+import requests
 
 app = Flask(__name__)
 
-usd = 11380.7 # 1 USD = 11380.7 UZS
+
+def get_data():
+    response = requests.get('https://nbu.uz/uz/exchange-rates/json/')
+
+    data= response.json()[-1]
+    usd = float(data['nbu_buy_price'])
+    return usd
+
 
 @app.route('/api/to-usd', methods=['GET'])
 def to_usd():
     """
     Convert to USD
-
     Returns:
         json: Converted amount
     
@@ -24,23 +31,21 @@ def to_usd():
                 "convertedCurrency": "USD"
             }
     """
-    
-    args=request.args
-    amount=float(args.get('amount',0))
-    
-   
-    print(amount)
+    usd = get_data()
+    args = request.args # get args from request
+    amount = float(args.get('amount', 0)) # convert to float
+    print(amount) # print amount
     return {
-                "amount": (amount),
-                "currency": "UZS",
-                "converted": round(amount/usd,2),
-                "convertedCurrency": "USD"
-    }
+            "amount": amount,
+            "currency": "UZS",
+            "converted": round(amount / usd, 2),
+            "convertedCurrency": "USD"
+        }
+
 @app.route('/api/to-uzs', methods=['GET'])
 def to_uzs():
     """
     Convert to UZS
-
     Returns:
         json: Converted amount
     
@@ -56,16 +61,17 @@ def to_uzs():
                 "convertedCurrency": "UZS"
             }
     """
-    args=request.args
-    amount=float(args.get('amount',0))
-    
-   
-    print(amount)
+    usd = get_data()
+    args = request.args # get args from request
+    amount = float(args.get('amount', 0)) # convert to float
+    print(amount) # print amount
     return {
-                "amount": (amount),
-                "currency": "USD",
-                "converted": round(amount*usd,2),
-                "convertedCurrency": "UZS"
-    }
+            "amount": amount,
+            "currency": "USD",
+            "converted": round(amount * usd, 2),
+            "convertedCurrency": "UZS"
+        }
+    
+
 if __name__ == '__main__':
-    app.run()    
+    app.run(debug=True)    
